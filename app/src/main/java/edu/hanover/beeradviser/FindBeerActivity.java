0009198bc3d0a08eae86ed8afa25f989f4897af3
Wilcolvan <vanderpohlw19@hanover.edu>
2018-05-17 11:20:56 -0400

    Initial commit

---
+++ app/src/main/java/edu/hanover/beeradviser/FindBeerActivity.java
@@ -0,0 +1,32 @@
+package edu.hanover.beeradviser;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.List;
+
+public class FindBeerActivity extends Activity {
+    private BeerExpert expert = new BeerExpert();
+
+    //Call when the user clicks a button
+    public void onClickFindBeer(View view) {
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+        brands.setText(beerType);
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList) {
+            brandsFormatted.append(brand).append('\n');
+        }
+        brands.setText(brandsFormatted);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+}
